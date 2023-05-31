853a4726cb13d4b289285d883faeed3abf31066d
Dongkeun Seo <dongkseo@cx1r5s2.42seoul.kr>
2023-06-01 02:44:24 +0900

    history

--- parse/syntax_interpretation.c
+++ parse/syntax_interpretation.c
@@ -6,7 +6,7 @@
 /*   By: dongkseo <dongkseo@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/26 20:41:10 by dongkseo          #+#    #+#             */
-/*   Updated: 2023/06/01 02:32:43 by dongkseo         ###   ########.fr       */
+/*   Updated: 2023/06/01 02:43:44 by dongkseo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -74,7 +74,8 @@ char	*remove_env_dquote(char *tmp)
 		return (NULL);
 	if (tmp[i] == '$' && (tmp[i + 1] == '\"' || tmp[i + 1] == '\''))
 	{
-		if (tmp[ft_strlen(tmp) - 2] != '\"' && tmp[ft_strlen(tmp) - 2] != '\'')
+		if (!tmp[i + 2] || (tmp[ft_strlen(tmp) - 1] != '\"' \
+		&& tmp[ft_strlen(tmp) - 1] != '\''))
 			return (NULL);
 		ret = ft_substr(&tmp[i + 2], 0, ft_strlen(&tmp[i + 2]) - 1);
 		free(tmp);
